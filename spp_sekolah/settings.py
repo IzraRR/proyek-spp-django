7aa1da7f8e8851a6aabc599d76129c4d85a2a786
IzraRR <wsdhij45@gmail.com>
2025-11-29 14:56:09 +0700

    Deploy

--- spp_sekolah/settings.py
+++ spp_sekolah/settings.py
@@ -130,6 +130,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGIN_URL = '/login/'
+
 # Mengarahkan pengguna ke URL root (/) setelah login berhasil
 LOGIN_REDIRECT_URL = '/'
 
